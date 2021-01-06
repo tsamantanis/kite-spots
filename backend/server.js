3b7063c25b0567f8b866e8914dc20955a0b67158
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-05 18:22:11 -0800

    Add backend server

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const uri = process.env.MONGODB_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+const connection = mongoose.connection;connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
+
+
+// app.use(require('./routes'));
