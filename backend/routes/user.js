b832369820f3777221d129bb42aae8a7b19d115a
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-05 18:24:07 -0800

    User login route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,24 @@ const { body, query, validationResult } = require('express-validator');
 const passport = require('passport');
 const User = mongoose.model('User');
 
+router.post('/login',[
+    body('user.email').isEmail(),
+    body('user.password').not().isEmpty(),
+], function(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    }
+
+    passport.authenticate('local', { session: false }, function(err, user, info) {
+        if (err) { return next(err); }
+
+        if (!user) { return res.sendStatus(403); }
+
+        return res.json({user: user.toAuthJSON()});
+    })(req, res, next);
+});
+
 router.post('/register', [
     body('user.email').isEmail(),
     body('user.password').isLength({min: 8}),
