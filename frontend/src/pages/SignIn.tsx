60028f864abb1e55bdae8b0a15020be4af9e7d33
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-07 11:49:39 -0800

    Add login form error handling frontend

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IonContent, IonGrid, IonPage, IonRow, IonCol } from '@ionic/react';
 
 import './LoginRegister.css';
 
 const SignIn: React.FC = () => {
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
+    const [errorMessage, setErrorMessage] = useState<string>('');
+
+    const login = (event: React.FormEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        if (!email.match('/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/')) {
+            setErrorMessage("Email is not valid");
+            return
+        }
+        if (password.length < 8) {
+            setErrorMessage("Password must be at least 8 characters");
+            return
+        }
+
+        // usePostLogin(email, password);
+    }
+
     return (
         <div className="container center-items">
             <img src="../../../assets/illustration/Globe.svg" alt="Globe" />
@@ -15,6 +33,8 @@ const SignIn: React.FC = () => {
                         type="email"
                         placeholder="example@example.com"
                         name="email"
+                        value={email}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setEmail(event.target.value); setErrorMessage('')}}
                     />
                 </div>
                 <div className="input-group">
@@ -23,9 +43,14 @@ const SignIn: React.FC = () => {
                         type="password"
                         placeholder="s0m3@-sTr0ng!-Pa5$"
                         name="password"
+                        minLength={8}
+                        value={password}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setPassword(event.target.value); setErrorMessage('')}}
                     />
+                    {errorMessage.length > 0 && <small>{errorMessage}</small>}
+
                 </div>
-                <button type="submit">Sign In</button>
+                <button onClick={login}>Sign In</button>
             </form>
         </div>
     );
