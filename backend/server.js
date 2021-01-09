2823babb427f9ddb2a5f8051ebe63630dc89fa27
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-08 17:24:57 -0800

    Allow incoming traffic from frontend link

--- backend/server.js
+++ backend/server.js
@@ -13,8 +13,11 @@ require('./config/passport');
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+    origin: ['http://localhost:3000', 'https://kite-spots.netlify.app'],
+    credentials: true,
+}
+app.use(cors(corsOptions));app.use(express.json());
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
