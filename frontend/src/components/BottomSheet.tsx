1d6a195f725e4506ca931eb0292975cf30a63941
tsamantanis <philip.tsamantanis@icloud.com>
2021-01-11 14:10:52 -0800

    Bottom sheet toggle revamp

--- frontend/src/components/BottomSheet.tsx
+++ frontend/src/components/BottomSheet.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { IonButton, IonCard, IonIcon } from '@ionic/react';
-import { close } from 'ionicons/icons';
+import { close, chevronUpOutline, chevronDownOutline } from 'ionicons/icons';
 import { createGesture, Gesture } from '@ionic/core';
 import './BottomSheet.css';
 
@@ -12,52 +12,27 @@ interface BottomSheetProps {
 const BottomSheet: React.FC<BottomSheetProps> = ({ content, toggleShowBottomSheet }) => {
     const drawerRef = useRef<HTMLIonCardElement | null>(null);
 
-    useEffect(() => {
-        let c = drawerRef.current as HTMLIonCardElement;
+    const toggleDrawer = () => {
+    let c = drawerRef.current as HTMLIonCardElement;
         if (typeof c !== 'undefined' && c !== null) {
-            const gesture: Gesture = createGesture({
-                el: c,
-                gestureName: "my-swipe",
-                direction: "y",
+            console.log(c);
+            if (c.dataset.open === "true") {
+                c.style.transition = ".5s ease-out";
+                c.style.transform = "";
+                c.dataset.open = "false";
+                c.children[1].classList.add('derotate');
+                c.children[1].classList.remove('rotate');
+            } else {
+                c.style.transition = ".5s ease-in";
+                c.style.transform = `translateY(${-350}px) `;
+                c.dataset.open = "true";
+                c.children[1].classList.add('rotate');
+                c.children[1].classList.remove('derotate');
 
-                onMove: event => {
-                    if (event.deltaY < -300) return;
-                    // closing with a downward swipe
-                    if (event.deltaY > 30) {
-                        c.style.transform = "";
-                        c.dataset.open = "false";
-                        return;
-                    }
-                    c.style.transform = `translateY(${event.deltaY}px)`;
-                },
-
-                onEnd: event => {
-                    c.style.transition = ".5s ease-out";
-                    if (event.deltaY < -30 && c.dataset.open !== "true") {
-                        c.style.transform = `translateY(${-350}px) `;
-                        c.dataset.open = "true";
-                        console.log("in on end");
-                    }
-                }
-            });
-            gesture.enable(true);
+            }
         }
-    }, []);
+    };
 
-    // const toggleDrawer = () => {
-    //     let c = drawerRef.current as HTMLIonCardElement;
-    //     if (typeof c !== 'undefined' && c !== null) {
-    //         if (c.dataset.open === "true") {
-    //             c.style.transition = ".5s ease-out";
-    //             c.style.transform = "";
-    //             c.dataset.open = "false";
-    //         } else {
-    //             c.style.transition = ".5s ease-in";
-    //             c.style.transform = `translateY(${-350}px) `;
-    //             c.dataset.open = "true";
-    //         }
-    //     }
-    // };
     return (
         <IonCard className="bottom-drawer" ref={drawerRef}>
             <IonIcon
@@ -67,11 +42,11 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ content, toggleShowBottomShee
                 color="light"
                 onClick={toggleShowBottomSheet}
             />
-            <div>
-                <IonButton
-                    size="small"
-                    // onClick={toggleDrawer}
-                    className="button-light"
+            <div className="button-container" onClick={toggleDrawer}>
+                <IonIcon
+                    color="primary"
+                    className="chevron"
+                    icon={chevronUpOutline}
                 />
             </div>
             { content }
